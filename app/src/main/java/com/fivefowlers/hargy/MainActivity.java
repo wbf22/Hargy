1ac01b3251210c5f95041a6b7e4dc7eb97f83368
brand <brandon@fivefowlers.com>
2021-09-25 18:18:49 -0600

    Initial commit

---
+++ app/src/main/java/com/fivefowlers/hargy/MainActivity.java
@@ -0,0 +1,21 @@
+package com.fivefowlers.hargy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO make a new connection
+    //TODO user info
+    //TODO make a group
+    //TODO see friends and past connections
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
