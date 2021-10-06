84b2c71e37fcc4d014f07f0e491fad973b5f197d
brand <brandon@fivefowlers.com>
2021-10-06 07:51:28 -0600

    Added new Activity, time to make database

--- app/src/main/java/com/fivefowlers/hargy/MainActivity.java
+++ app/src/main/java/com/fivefowlers/hargy/MainActivity.java
@@ -3,6 +3,8 @@ package com.fivefowlers.hargy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView newConnectionButton = (ImageView)findViewById(R.id.newConnection);
+        newConnectionButton.setOnClickListener(v -> {
+            //go to make a new connection activity
+        });
+
+        ImageView friends = (ImageView)findViewById(R.id.friends);
+        friends.setOnClickListener(v -> {
+            //go to friends page
+        });
+
     }
 }
---
+++ app/src/main/java/com/fivefowlers/hargy/NewConnectionActivity.java
@@ -0,0 +1,16 @@
+package com.fivefowlers.hargy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewConnectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_connection);
+
+
+    }
+}
